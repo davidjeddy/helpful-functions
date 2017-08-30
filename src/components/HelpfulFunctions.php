cd14646893487bb4ce03a561de30b0d047cb07ee
David J Eddy <me@davidjeddy.com>
2017-08-30 10:47:33 -0400

    ADDED 3 helpful functions UPDATED composer.json; removed Yii2 dependency CHANGED namespacing to simply dje\*

--- src/components/HelpfulFunctions.php
+++ src/components/HelpfulFunctions.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace dje\components;
+namespace dje;
 
 /**
  * Class HelpfulFunctions
@@ -49,5 +49,66 @@ class HelpfulFunctions extends \yii\base\Component
         }
 
         return $obj;
+    }
+
+    /**
+     * Basically, convert 'jpeg' to 'jpg', all other file extensions are three letter already.
+     *
+     * @param string $fileName
+     *
+     * @return string
+     */
+    public static function sanitizeFileExt(string $fileName = null) : string
+    {
+        $returnData = explode('.', $fileName);
+        $returnData = end($returnData);
+
+        // remove query string from file extension
+        if (strpos($returnData, '?') > 0) {
+            $returnData = explode('?', $returnData)[0];
+        }
+
+        if ($returnData === 'jpeg') {
+            $returnData = 'jpg';
+        }
+
+        return $returnData;
+    }
+
+    /**
+     * @source http://stackoverflow.com/questions/5147691/return-all-array-elements-except-for-a-given-key
+     * @param       $array
+     * @param array $excludeKeys
+     *
+     * @return mixed
+     */
+    public static function arrayExclude($array, array $excludeKeys)
+    {
+        foreach ($excludeKeys as $key) {
+            unset($array[$key]);
+        }
+        return $array;
+    }
+
+    /**
+     * @param string $url
+     * @param string $target
+     * @return bool
+     */
+    public static function curlGET(string $url, string $target): bool
+    {
+        $ch = curl_init($url);
+        $fp = fopen($target, 'wb');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        $response = fclose($fp);
+
+        if ($response !== true) {
+            throw new \Error(curl_errno($ch));
+        }
+
+        return $response;
     }
 }
